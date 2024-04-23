92a39ecbfc244bde11ac75cbe52100c00e591606
Chris Sutton <chrissutton1789@gmail.com>
2024-04-23 19:15:54 +0000

    added stripe elements

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51P8nXqRt0SH89RWsYbr6snk94efYZCTYQn8rkQ0Fig47eqbivFmGokPJapLCt8giOVOJqa3Q7UVYsNBa8GMtcj9U00DsY8VMdo', # noqa
+        'client_secret': 'test client secret'
     }
 
     return render(request, template, context)
