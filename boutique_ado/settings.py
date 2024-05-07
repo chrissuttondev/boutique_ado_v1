b5e67d5035647cffbd9210082958e78c470ff3f3
Chris Sutton <chrissutton1789@gmail.com>
2024-05-07 16:55:13 +0000

    Deployment 003

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-i4$b+$%vypr0vs5yo697=_ajzo!d_m7*og)i(d5lw5nvshb1*u' # noqa
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
